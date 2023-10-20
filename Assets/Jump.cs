90d68c51dc0fbe0bec4d82e4ef9dfb27bf38257e
jun Kim (Daniel) <junkim@myyahoo.com>
2023-10-19 21:30:54 -0400

    Cut off white background for Tony and tried to add jump animationd

---
+++ Assets/Jump.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Setting keys to activate jump.
+        if(Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidbody.velocity = Vector2.up * 5;
+        }
+        
+        
+    }
+}
